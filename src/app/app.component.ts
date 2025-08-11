ad4a14b3c88d84ec609c1b9a3003fd43757cb24a
Amaan-Radhanpura <amaanradhanpura5@gmail.com>
2025-08-11 19:29:20 +0530

    11-8-25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,13 +18,13 @@ export class AppComponent implements OnInit{
 constructor(private service :DialougService){}
   viewDate:Date=new Date();
   list:boolean=false;
-  displayedColumns=['Title','Start','StartTime','End']
+  displayedColumns=['Title','Start','End']
   filteredData=[]
   //This is an predifned in the angular-calendar library to add any event in the calendar
   events:CalendarEvent[]=[
     {
       start:new Date(2025,7,7,10,0),
-      end:new Date(2025,7,7,12,0),
+      end:new Date(2025,7,7,1,0),
       title:'Patient A'
       
     },
@@ -35,14 +35,36 @@ constructor(private service :DialougService){}
     }
   ]
 
+  
   ngOnInit(): void {
     this.events = JSON.parse(localStorage.getItem('event'));
     this.filteredData=this.events
   }
 
+
+   monthEvents() {
+    return this.events.map(e => {
+    let start = new Date(e.start);
+    let end = new Date(e.end);
+ 
+    if (start > end) {
+      const temp = start;
+      start = end;
+      end = temp;
+    }
+ 
+    return {
+      ...e,
+      start,
+      end
+    };
+  });
+  }
+
   //This Function is to set the view that wether we need the month,Week or Day View
   setView(view:CalendarView){
     this.view=view;
+    this.list=false;
   }
 
   //This Function is mainly to get back to present day
@@ -66,11 +88,11 @@ constructor(private service :DialougService){}
   private changeDate(no:number):Date{
     switch(this.view){
       case CalendarView.Month:
+        this.list=false;
         return addMonths(this.viewDate,no);
       case CalendarView.Week:
+        this.list=false;
         return addWeeks(this.viewDate,no);
-      case CalendarView.Day:
-        return addDays(this.viewDate,no);
       default:
         return this.viewDate;
     }
@@ -95,16 +117,36 @@ constructor(private service :DialougService){}
   }
 
   listTable(){
-    console.log("First click");
     this.list=true;
     console.log(this.list);  
   }
-  searchText:string=''
 
+  
+  //Function to serarch based on Title
+  searchText:string=''
   Title(event:any){
     if(event){
       this.searchText=event.target.value
       this.filteredData=this.events.filter(item=>item.title.toLowerCase().includes(this.searchText.toLowerCase()))
     }
   }
+
+  //Function to serarch based on Start
+  search:Date=new Date()
+  Start(event:any){
+    console.log(event)
+    if(event){
+      this.search=event.target.value
+      this.filteredData=this.events.filter(item=>item.start==this.search)
+    }
+  }
+
+  End(event:any){
+    console.log(event)
+     if(event){
+      this.search=event.target.value
+      this.filteredData=this.events.filter(item=>item.end==this.search)
+    }
+  }
+
 }
